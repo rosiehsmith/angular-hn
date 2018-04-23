4659042e22b2ec4be30c738a7c648b4d8eda0e42
rosiehighsmith <rosie.highsmith@gmail.com>
2018-04-22 20:40:53 -0700

    populate items with hackernews api data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 import { HackerNewsAPIService } from './hackernews-api.service';
 import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
+import { MomentModule } from 'ngx-moment';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { StoriesComponent } from './stories/stories.component';
 import { FooterComponent } from './footer/footer.component';
 import { ItemComponent } from './item/item.component';
-
+import { DomainPipe } from './domain.pipe';
 
 @NgModule({
   declarations: [
@@ -17,12 +18,14 @@ import { ItemComponent } from './item/item.component';
     HeaderComponent,
     StoriesComponent,
     FooterComponent,
-    ItemComponent
+    ItemComponent,
+    DomainPipe
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     HttpModule,
+    MomentModule
   ],
   providers: [
     HackerNewsAPIService,
